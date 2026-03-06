e1bfaf8d93da4f0ed54c9aa544d8036f44a91da0
Raghunath <rockersraghu09@gmail.com>
2026-03-06 23:37:13 +0530

    Add files via upload

---
+++ primitiveDataTypes.js
@@ -0,0 +1,11 @@
+let userName = 'TestUser';
+let organization = 'Google';
+let contactNumber = 9876543210;
+let knowsAutomation = true;
+let usesPlaywright;
+
+console.log("userName is : ", userName , " and it is of type :", typeof userName);
+console.log("Organization  is : ", organization , "  and it is of type :", typeof organization);
+console.log("ContactNumber  is : ", contactNumber , "  and it is of type :", typeof contactNumber);
+console.log("Does the user know Automation : ", knowsAutomation , "  and it is of type :", typeof knowsAutomation);
+console.log("Does the user use Playwright : ", usesPlaywright , "  and it is of type :", typeof usesPlaywright);
